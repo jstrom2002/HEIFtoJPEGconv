86c9dde9c6e5eb627224dd184dbeeb7552ae28f8
JH Strom <jstrom2002@hotmail.com>
2022-05-31 10:22:05 -0600

    add png file conversion functionality

--- include/heif_converter.h
+++ include/heif_converter.h
@@ -4,9 +4,22 @@
 #include <csetjmp>
 #include <filesystem>
 #include <stdio.h>
+#include <jpeglib.h>
 
 namespace HEIFtoJPEG
 {
+	struct ErrorHandler
+	{
+		struct jpeg_error_mgr pub;  /* "public" fields */
+		jmp_buf setjmp_buffer;  /* for return to caller */
+	};
+
+	static bool HasExifMetaData(const struct heif_image_handle* handle);
+	static uint8_t* GetExifMetaData(const struct heif_image_handle* handle, size_t* size);
+	static void UpdateDecodingOptions(const struct heif_image_handle* handle, struct heif_decoding_options* options);
+
+	enum FORMAT { UNKNOWN = 0u, JPG = 1u, PNG = 2u };
+
 	class heif_converter
 	{
 	private:
@@ -14,9 +27,18 @@ namespace HEIFtoJPEG
 		int option_aux = 0;
 		int option_no_colons = 0;
 		int quality_ = 90;
+		FORMAT format = UNKNOWN;
+		std::string output_dir = "";
+		std::string output_filename = "";
+		std::string filename = "";
+		std::vector<heif_item_id> image_IDs;
+		size_t image_index = 1;  // Image filenames are "1" based.
+		struct heif_error err;
+		struct heif_image_handle* handle = nullptr;
+		struct heif_image* image = nullptr;
 
 	public:
-		void ConvertToJPEG(const char* input_filename);
+		void Convert(const char* filename, int format, float quality_ = 90);
 
 		~heif_converter()
 		{
@@ -26,27 +48,60 @@ namespace HEIFtoJPEG
 
     private:
 
+		void ConvertToJPEG();
+		void ConvertToPNG();
+
 		void HEIF_CONVERTER_EXCEPTION(std::string str) {
 			throw new std::exception(str.c_str());
 		}
 
+		void appendFileExt(std::string& str)
+		{
+			switch (format) {
+			case JPG:
+				str += ".jpg";
+				break;
+			case PNG:
+				str += ".png";
+				break;
+			}
+		}
+
 		heif_colorspace colorspace(bool has_alpha) const
 		{
-			return heif_colorspace_YCbCr;
+			switch (format) {
+			case JPG:
+				return heif_colorspace_YCbCr;
+			case PNG:
+				return heif_colorspace_RGB;
+			}
+
+			return heif_colorspace_undefined;
 		}
 
 		heif_chroma chroma(bool has_alpha, int bit_depth) const
 		{
-			return heif_chroma_420;
+			switch (format) {
+			case JPG:
+				return heif_chroma_420;
+			case PNG:
+				if (bit_depth == 8) {
+					if (has_alpha)
+						return heif_chroma_interleaved_RGBA;
+					else
+						return heif_chroma_interleaved_RGB;
+				}
+				else {
+					if (has_alpha)
+						return heif_chroma_interleaved_RRGGBBAA_BE;
+					else
+						return heif_chroma_interleaved_RRGGBB_BE;
+				}
+			}
+
+			return heif_chroma_undefined;
 		}
 
-		void UpdateDecodingOptions(const struct heif_image_handle* handle,
-			struct heif_decoding_options* options) const;
-
-		bool Encode(const struct heif_image_handle* handle,
-			const struct heif_image* image, const std::string& filename);
-
-		static bool HasExifMetaData(const struct heif_image_handle* handle);
-        static uint8_t* GetExifMetaData(const struct heif_image_handle* handle, size_t* size);
+		bool Encode(const struct heif_image_handle* handle, const struct heif_image* image, const std::string& filename);
 	};
 }
